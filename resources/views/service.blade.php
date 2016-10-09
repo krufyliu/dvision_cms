b1ffacc5de30897eae4c1785e5ff426168b003db
tkvern <tkvern@qq.com>
2016-10-09 17:10:27 +0800

    完成1.0.0版本细节修改

--- resources/views/service.blade.php
+++ resources/views/service.blade.php
@@ -32,8 +32,7 @@
                     <img src="/image/serve01.png" alt="..." width="120" height="120">
                     <div class="caption">
                         <h4>专业设备租赁</h4>
-                        <br>
-                        <p>
+                        <p class="text-muted">
                             自主研发的全景影视级拍摄设备“ AURA ”的租赁,买卖
                         </p>
                     </div>
@@ -42,8 +41,7 @@
                     <img src="/image/serve03.png" alt="..." width="120" height="120">
                     <div class="caption">
                         <h4>后期剪辑 字幕</h4>
-                        <br>
-                        <p>
+                        <p class="text-muted">
                             全景视频拼接、剪辑、调色、字幕等一体化服务
                         </p>
                     </div>
@@ -52,8 +50,7 @@
                     <img src="/image/serve02.png" alt="..." width="120" height="120">
                     <div class="caption">
                         <h4>团队拍摄</h4>
-                        <br>
-                        <p>
+                        <p class="text-muted">
                             业内顶尖的VR拍摄团队，国际知名 VR导演，艺术指导
                         </p>
                     </div>
@@ -62,8 +59,7 @@
                     <img src="/image/serve04.png" alt="..." width="120" height="120">
                     <div class="caption">
                         <h4>软件</h4>
-                        <br>
-                        <p>
+                        <p class="text-muted">
                             独立知识产权的可交互式VR播放器
                         </p>
                     </div>
@@ -72,8 +68,7 @@
                     <img src="/image/serve05.png" alt="..." width="120" height="120">
                     <div class="caption">
                         <h4>视觉特效</h4>
-                        <br>
-                        <p>
+                        <p class="text-muted">
                             VR视觉特效以及整体包装
                         </p>
                     </div>
@@ -82,8 +77,7 @@
                     <img src="/image/serve06.png" alt="..." width="120" height="120">
                     <div class="caption">
                         <h4>直播</h4>
-                        <br>
-                        <p>
+                        <p class="text-muted">
                             多范围场景直播，视频清晰流畅，无延时、无卡顿
                         </p>
                     </div>
