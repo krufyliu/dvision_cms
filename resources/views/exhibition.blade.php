0b539fda4b5cb81930edf75c25c2d8b196b52023
tkvern <tkvern@qq.com>
2016-12-13 13:17:52 +0800

    添加展示页面

---
+++ resources/views/exhibition.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0" />
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+    <title>量子视觉科技有限公司</title>
+    <meta name="keywords" content="量子视觉科技有限公司,量子视觉,VR视频,全景,VR服务,DKvision,vision">
+    <meta name="description" content="DKvision致力于打造下一代虚拟现实的完整解决方案">
+    <style type="text/css">
+      body {
+        margin: 0px;
+      }
+    </style>
+</head>
+<body>
+  <div style="width: 100%; position: relative;">
+    <img src="/image/h5.png" width="100%">
+    <a href="/contact">
+      <div style="height: 25vh; width: 100%; position: absolute; bottom: 0px;"></div>
+    </a>
+  </div>
+</body>
