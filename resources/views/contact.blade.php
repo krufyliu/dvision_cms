939d6e9f068ab966acecf80790df160e614634f0
tkvern <tkvern@qq.com>
2016-12-22 16:33:57 +0800

    修改留言页面

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,14 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-<div id="carousel-generic" class="carousel slide" data-ride="carousel">
+<div id="carousel-generic" class="carousel slide hidden-xs hidden-sm" data-ride="carousel" style="height: 100vh; min-height: 800px; background: url(/image/contactus_high_01.jpg) no-repeat; background-size: cover;">
     <div class="carousel-inner" role="listbox">
-        <div class="item active">
-            <img src="/image/contactus_high_01.jpg" class="hidden-xs hidden-sm" alt="contact" width="100%">
-            <img src="/image/contactus_low_01.jpg" class="visible-xs visible-sm" alt="contact" width="100%">
+        <!-- <div class="item active">
+            <img src="/image/contactus_high_01.jpg" class="hidden-xs hidden-sm" alt="contact" width="100%" style="min-height: 100%;">
+            <img src="/image/contactus_low_01.jpg" class="visible-xs visible-sm" alt="contact" width="100%" style="min-height: 100%;">
             <div class="carousel-caption">
             </div>
-        </div>
+        </div> -->
+    </div>
+</div>
+<div id="carousel-generic" class="carousel slide visible-xs visible-sm" data-ride="carousel" style="height: 165vh; background: url(/image/contactus_high_01.jpg) no-repeat; background-size: cover;">
+    <div class="carousel-inner" role="listbox">
     </div>
 </div>
 <div style="position: absolute; width: 100%; top: 10%; left: 0px; color: #fff; opacity: 0.9;">
@@ -20,7 +24,7 @@
 
                     <div class="form-group">
                         <div class="col-sm-12 text-center">
-                            <h2 style="font-weight: 300;">联系我们</h2>
+                            <h2 style="font-weight: 300;">联系我们/Contact</h2>
                             <!-- <p>如果您想要更深入的了解我们的产品和服务<br>请填写下面的表格，我们将很快与您取得联系</p> -->
                         </div>
                     </div>
@@ -40,39 +44,39 @@
                     </div>
 
                     <div class="form-group" style="margin-top: 40px;">
-                        <label for="" class="col-sm-2 control-label"><span style="color: red;">*</span>&nbsp;公司或个人名称</label>
-                        <div class="col-sm-8">
+                        <label for="" class="col-sm-3 col-md-3 control-label" style="padding-top: 0px;"><span style="color: red;">*</span>&nbsp;公司或个人名称<br>Company or individual name</label>
+                        <div class="col-sm-7">
                             <input type="text" name="name" class="form-control" required="">
                         </div>
                     </div>
                     <div class="form-group">
-                        <label for="" class="col-sm-2 control-label"><span style="color: red;">*</span>&nbsp;联系电话</label>
-                        <div class="col-sm-8">
+                        <label for="" class="col-sm-3 col-md-3 control-label" style="padding-top: 0px;"><span style="color: red;">*</span>&nbsp;联系电话<br>Phone</label>
+                        <div class="col-sm-7">
                             <input type="phone" name="phone" class="form-control" required="">
                         </div>
                     </div>
                     <div class="form-group">
-                        <label for="" class="col-sm-2 control-label"><span style="color: red;">*</span>&nbsp;联系邮箱</label>
-                        <div class="col-sm-8">
+                        <label for="" class="col-sm-3 col-md-3 control-label" style="padding-top: 0px;"><span style="color: red;">*</span>&nbsp;联系邮箱<br>E-mail</label>
+                        <div class="col-sm-7">
                             <input type="email" name="email" class="form-control" required="">
                         </div>
                     </div>
                     <div class="form-group">
-                        <label for="" class="col-sm-2 control-label"><span style="color: red;">*</span>&nbsp;留言内容</label>
-                        <div class="col-sm-8">
+                        <label for="" class="col-sm-3 col-md-3 control-label" style="padding-top: 0px;"><span style="color: red;">*</span>&nbsp;留言内容<br>Content</label>
+                        <div class="col-sm-7">
                             <textarea class="form-control" name="content" rows="8" required=""></textarea>
                         </div>
                     </div>
                     <div class="form-group">
-                        <label for="" class="col-sm-2 control-label"></label>
-                        <div class="col-sm-8">
+                        <label for="" class="col-sm-3 col-md-3 control-label"></label>
+                        <div class="col-sm-7">
                             {!! Geetest::render() !!}
                         </div>
                     </div>
 
                     <div class="form-group">
-                        <div class="col-sm-offset-2 col-sm-8">
-                            <button type="submit" class="btn btn-primary" style="width: 100%;">留言</button>
+                        <div class="col-sm-offset-3 col-sm-7">
+                            <button type="submit" class="btn btn-primary" style="width: 100%;">留言/Leaving a message</button>
                         </div>
                     </div>
                 </form>
