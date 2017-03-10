146f67e22ccd4b687d0d9dbc6e2ab21fea215e6a
tkvern <tkvern@qq.com>
2017-03-10 10:02:09 +0800

    update zshuang

--- resources/views/zshuang.blade.php
+++ resources/views/zshuang.blade.php
@@ -9,17 +9,12 @@
   <tr>
     <td width="1449"><h1 align="center">CUHK Badminton Game Data Set</h1>
       <p align="center">&nbsp;</p>
-      <p align="center"><img src="/image/badminton.jpg" width="647" height="432" alt="" /></p>
+      <p align="center"><img src="/image/badminton.jpg" width="847" height="432" alt="" /></p>
     <h2 align="left">Download</h2>
-    <p align="left">CUHK Badminton game data set is for research on badminton tracking and sport event analysis. Currently it includes 30 badminton game videos with each about 5 minutes long. It is collected from professional competition video clips or recorded by a stationary camera. The size of the scene is 1920x1080. </p>
-  <p align="left">
-  <a href="/storage/Dataset.rar">
-  CUHK BGDS download link</a>
-  </p>
-  <p align="left">In order to evaluate the performance of badminton tracking accurate rate on this data set, ground truth of shuttlecock are manually labeled. It can be downloaded below. A readme file provides the instructions of how to use it.</p>
-  <p align="left"><a href="/storage/GroundTruth.rar">CUHK BGDS Ground truth download link</a></p>
+    <p align="left">CUHK Badminton game data set is for research on badminton tracking and sport event analysis. Currently it includes 30 badminton game videos with each about 5 minutes long. It is collected from professional competition video clips or recorded by a stationary camera. In order to evaluate the performance of badminton tracking accurate rate on this data set, ground truth of shuttlecock are manually labeled. It can be downloaded below. A readme file provides the instructions of how to use it.</p>
+  <p align="left"><a href="https://pan.baidu.com/s/1nvLQnOX">CUHK BGDS download link</a></p>
   <p align="left">The label and tracking program is packed and can be downloaded as following link:</p>
-  <p align="left"><a href="/storage/Program.rar">Executable Program</a></p>
+  <p align="left"><a href="/storage/Release_v01.rar">Executable Program</a></p>
 
   <h2>Tracking result</h2>
   <p align="center"><img src="/image/result.png" width="739" height="247" alt="" /></p>
