c49a21f5ad9d2505906cdb3345f647ae3d9d6521
tkvern <tkvern@qq.com>
2017-03-17 20:00:09 +0800

    修改zshuang

--- resources/views/zshuang.blade.php
+++ resources/views/zshuang.blade.php
@@ -11,10 +11,9 @@
       <p align="center">&nbsp;</p>
       <p align="center"><img src="/image/badminton.jpg" width="847" height="432" alt="" /></p>
     <h2 align="left">Download</h2>
-    <p align="left">CUHK Badminton game data set is for research on badminton tracking and sport event analysis. Currently it includes 30 badminton game videos with each about 5 minutes long. It is collected from professional competition video clips or recorded by a stationary camera. In order to evaluate the performance of badminton tracking accurate rate on this data set, ground truth of shuttlecock are manually labeled. It can be downloaded below. A readme file provides the instructions of how to use it.</p>
-  <p align="left"><a href="https://pan.baidu.com/s/1nvLQnOX">CUHK BGDS download link</a></p>
-  <p align="left">The label and tracking program is packed and can be downloaded as following link:</p>
-  <p align="left"><a href="/storage/Release_v01.rar">Executable Program</a></p>
+    <p align="left">CUHK Badminton game data set is for research on badminton tracking and sport event analysis. Currently it includes 30 badminton game videos with each about 5 minutes long. It is collected from professional competition video clips or recorded by a stationary camera. In order to evaluate the performance of badminton tracking accurate rate on this data set, ground truth of shuttlecock are manually labeled. The label and tracking program is packed and can be downloaded as following link. A readme file provides the instructions of how to use it.</p>
+  <p align="left"><a href="https://pan.baidu.com/s/1nvLQnOX">CUHK BGDS download link(BaiduYun)</a></p>
+  <p align="left"><a href="https://drive.google.com/drive/folders/0Bxi1RhPzmVpzbWhQRVJ5QmR3T3c?usp=sharing">CUHK BGDS download link(GoogleDrive)</a></p>
 
   <h2>Tracking result</h2>
   <p align="center"><img src="/image/result.png" width="739" height="247" alt="" /></p>
