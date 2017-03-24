ee7405cda2d1a0ce755a0f1abb9dc86d5295e911
tkvern <tkvern@qq.com>
2017-03-24 17:11:07 +0800

    完成图片处理，服务页面

--- resources/views/service.blade.php
+++ resources/views/service.blade.php
@@ -13,5 +13,101 @@
                 </div>
             </div>
         </div>
+        <div class="serve section-02">
+            <div class="container">
+                <div class="content">
+                    <div class="text_panel">
+                        <ul class="figs">
+                            <li class="fig">
+                                <div class="icons">
+                                    <img src="img/service_sec02_01.png" alt="service_sec02_01" width="128"  draggable="false">
+                                </div>
+                                
+                                <p class="fig_value">
+                                    专业设备租赁
+                                </p>
+                                <span class="fig_title">
+                                    自主研发的全景影视级拍摄设备“ AURA ”的销售，租赁
+                                </span>
+                            </li>
+                            <li class="fig">
+                                <div class="icons">
+                                    <img src="img/service_sec02_02.png" alt="service_sec02_02" width="128"  draggable="false">
+                                </div>
+                                
+                                <p class="fig_value">
+                                    云平台
+                                </p>
+                                <span class="fig_title">
+                                    全球领先的VR制作及三维重建SaaS云平台
+                                </span>
+                            </li>
+                            <li class="fig">
+                                <div class="icons">
+                                    <img src="img/service_sec02_03.png" alt="service_sec02_03" width="128"  draggable="false">
+                                </div>
+                                
+                                <p class="fig_value">
+                                    软件
+                                </p>
+                                <span class="fig_title">
+                                    独立知识产权的可交互式VR播放器
+                                </span>
+                            </li>
+                            
+                        </ul>
+                        <ul class="figs">
+                            <li class="fig">
+                                <div class="icons">
+                                    <img src="img/service_sec02_04.png" alt="service_sec02_04" width="128"  draggable="false">
+                                </div>
+                                
+                                <p class="fig_value">
+                                    VR直播
+                                </p>
+                                <span class="fig_title">
+                                    多范围场景直播，视频清晰流畅，低延时、真3D
+                                </span>
+                            </li>
+                            <li class="fig">
+                                <div class="icons">
+                                    <img src="img/service_sec02_05.png" alt="service_sec02_05" width="128"  draggable="false">
+                                </div>
+                                
+                                <p class="fig_value">
+                                    影视级VR制作
+                                </p>
+                                <span class="fig_title">
+                                    全景视频拼接、剪辑、调色、特效等一体化服务
+                                </span>
+                            </li>
+                            <li class="fig">
+                                <div class="icons">
+                                    <img src="img/service_sec02_06.png" alt="service_sec02_06" width="128"  draggable="false">
+                                </div>
+                                
+                                <p class="fig_value">
+                                    团队拍摄
+                                </p>
+                                <span class="fig_title">
+                                    业内顶尖的VR拍摄团队，国际知名 VR导演，艺术指导
+                                </span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="serve section-03">
+            <div class="container">
+                <div class="content">
+                    <img src="img/service_sec03_01.jpg" width="1190" alt="service_sec03_01" class="service_sec03_01" draggable="false">
+                    <div class="text_panel">
+                        <span class="title">合作伙伴</span>
+                        <p class="txt">COOPERATIVE PARTNER</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
