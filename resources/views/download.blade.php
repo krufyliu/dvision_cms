406bc27c29e476d7f21a4fa5afbf8ce549a6bd40
tkvern <tkvern@qq.com>
2017-07-04 17:33:39 +0800

    修改配置要求

--- resources/views/download.blade.php
+++ resources/views/download.blade.php
@@ -17,7 +17,7 @@
                                     <img src="{{ $download->aura['studio']['image_url'] }}" alt="aura_sec06_04" width="238" draggable="false">
                                 </div>
                                 <span class="title">流畅剪辑<br>云端精拼的专业体验</span>
-                                <p class="txt">推荐配置要求：<br>第五代智能英特尔酷睿i5处理器以上<br>8G内存、7200转机械硬盘或SSD固态硬盘<br>Windows10或MacOS 10.12+操作系统<br> NVIDIA GeForce系列 GTX 960以上(Windows)<br>最佳使用分辨率2560x1600(全屏)</p>
+                                <p class="txt">推荐配置要求：<br>第五代智能英特尔酷睿i5处理器以上<br>16G内存、7200转机械硬盘或SSD固态硬盘<br>Windows10或MacOS 10.12+操作系统<br> NVIDIA GeForce系列 GTX 960以上(Windows)<br>最佳使用分辨率2560x1600(全屏)</p>
                                 <div class="links">
                                   <a href="{{ $download->aura['studio']['win']['url'] }}" title="{{ $download->aura['studio']['win']['title'] }}" class="link"  target="_blank" rel="nofollow"><img src="/img/icon-win.png" width="17" class="ics" draggable="false">&nbsp;Win10</a>
                                   <a href="{{ $download->aura['studio']['mac']['url'] }}" title="{{ $download->aura['studio']['mac']['title'] }}" class="link" target="_blank" rel="nofollow"><img src="/img/icon-mac.png" width"17" class="ics" draggable="false">&nbsp;MacOS</a>
@@ -31,7 +31,7 @@
                                     <img src="{{ $download->aura['work']['image_url'] }}" alt="aura_sec06_03" width="238" draggable="false">
                                 </div>
                                 <span class="title">实时监控<br>所有镜头  实时操控</span>
-                                <p class="txt">推荐配置要求：<br>第五代智能英特尔酷睿i5处理器以上<br>8G内存、7200转机械硬盘或SSD固态硬盘<br>Windows10或MacOS 10.12+操作系统<br> NVIDIA GeForce系列 GTX 960以上(Windows)<br>最佳使用分辨率2560x1600(全屏)</p>
+                                <p class="txt">推荐配置要求：<br>第五代智能英特尔酷睿i5处理器以上<br>16G内存、7200转机械硬盘或SSD固态硬盘<br>Windows10或MacOS 10.12+操作系统<br> NVIDIA GeForce系列 GTX 960以上(Windows)<br>最佳使用分辨率2560x1600(全屏)</p>
                                 <div class="links">
                                   <a href="{{ $download->aura['work']['win']['url'] }}" title="{{ $download->aura['work']['win']['title'] }}" class="link"  target="_blank" rel="nofollow"><img src="/img/icon-win.png" width="17" class="ics" draggable="false">&nbsp;Win10</a>
                                   <a href="{{ $download->aura['work']['mac']['url'] }}" title="{{ $download->aura['work']['mac']['title'] }}" class="link"  target="_blank" rel="nofollow"><img src="/img/icon-mac.png" width"17" class="ics" draggable="false">&nbsp;MacOS</a>
