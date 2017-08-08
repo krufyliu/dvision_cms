065e2b45dfb8267cdc5f7cba2e0bd50b9a2207ce
tkvern <tkvern@qq.com>
2017-08-08 10:49:25 +0800

    添加下载属性

--- resources/views/download.blade.php
+++ resources/views/download.blade.php
@@ -19,8 +19,8 @@
                                 <span class="title">流畅剪辑<br>云端精拼的专业体验</span>
                                 <p class="txt">推荐配置要求：<br>第七代智能英特尔酷睿i5处理器以上<br>16G内存、7200转机械硬盘或SSD固态硬盘<br>Windows10或MacOS 10.12+操作系统<br> NVIDIA GeForce系列 GTX 960以上(Windows)<br>最佳使用分辨率2560x1600(全屏)</p>
                                 <div class="links">
-                                  <a href="{{ $download->aura['studio']['win']['url'] }}" title="{{ $download->aura['studio']['win']['title'] }}" class="link"  target="_blank" rel="nofollow"><img src="/img/icon-win.png" width="17" class="ics" draggable="false">&nbsp;Win10</a>
-                                  <a href="{{ $download->aura['studio']['mac']['url'] }}" title="{{ $download->aura['studio']['mac']['title'] }}" class="link" target="_blank" rel="nofollow"><img src="/img/icon-mac.png" width"17" class="ics" draggable="false">&nbsp;MacOS</a>
+                                  <a href="{{ $download->aura['studio']['win']['url'] }}" title="{{ $download->aura['studio']['win']['title'] }}" class="link"  target="_blank" rel="nofollow" download><img src="/img/icon-win.png" width="17" class="ics" draggable="false">&nbsp;Win10</a>
+                                  <a href="{{ $download->aura['studio']['mac']['url'] }}" title="{{ $download->aura['studio']['mac']['title'] }}" class="link" target="_blank" rel="nofollow" download><img src="/img/icon-mac.png" width"17" class="ics" draggable="false">&nbsp;MacOS</a>
                                   {{-- <a href="/contact" class="link" rel="nofollow">使用必读</a> --}}
                                 </div>
                                 <p class="txt small">{{ $download->aura['studio']['version'] }}</p>
@@ -33,8 +33,8 @@
                                 <span class="title">实时监控<br>所有镜头  实时操控</span>
                                 <p class="txt">推荐配置要求：<br>第七代智能英特尔酷睿i5处理器以上<br>16G内存、7200转机械硬盘或SSD固态硬盘<br>Windows10或MacOS 10.12+操作系统<br> NVIDIA GeForce系列 GTX 960以上(Windows)<br>最佳使用分辨率2560x1600(全屏)</p>
                                 <div class="links">
-                                  <a href="{{ $download->aura['work']['win']['url'] }}" title="{{ $download->aura['work']['win']['title'] }}" class="link"  target="_blank" rel="nofollow"><img src="/img/icon-win.png" width="17" class="ics" draggable="false">&nbsp;Win10</a>
-                                  <a href="{{ $download->aura['work']['mac']['url'] }}" title="{{ $download->aura['work']['mac']['title'] }}" class="link"  target="_blank" rel="nofollow"><img src="/img/icon-mac.png" width"17" class="ics" draggable="false">&nbsp;MacOS</a>
+                                  <a href="{{ $download->aura['work']['win']['url'] }}" title="{{ $download->aura['work']['win']['title'] }}" class="link"  target="_blank" rel="nofollow" download><img src="/img/icon-win.png" width="17" class="ics" draggable="false">&nbsp;Win10</a>
+                                  <a href="{{ $download->aura['work']['mac']['url'] }}" title="{{ $download->aura['work']['mac']['title'] }}" class="link"  target="_blank" rel="nofollow" download><img src="/img/icon-mac.png" width"17" class="ics" draggable="false">&nbsp;MacOS</a>
                                   {{-- <a href="/contact" class="link"  target="_blank" rel="nofollow">使用必读</a> --}}
                                 </div>
                                 <p class="txt small">{{ $download->aura['work']['version'] }}</p>
@@ -47,7 +47,7 @@
                                 <span class="title">固件下载</span>
                                 <p class="txt">我们会不断完善AURA的功能体验，您可以下载后<br>使用AURA WORK对AURA相机进行升级。</p>
                                 <div class="links">
-                                  <a href="{{ $download->aura['firmware']['url'] }}" title="{{ $download->aura['firmware']['title'] }}" class="link"  target="_blank" rel="nofollow">&nbsp;立即下载</a>
+                                  <a href="{{ $download->aura['firmware']['url'] }}" title="{{ $download->aura['firmware']['title'] }}" class="link"  target="_blank" rel="nofollow" download>&nbsp;立即下载</a>
                                   {{-- <a href="/contact" class="link"  target="_blank" rel="nofollow">使用必读</a> --}}
                                 </div>
                                 <p class="txt small">{{ $download->aura['firmware']['version'] }}</p>
